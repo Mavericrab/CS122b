a75515f23f30f142978a82bcecbb29b34b541b6e
donglil <donglil@uci.edu>
2016-03-31 15:51:49 -0700

    Added files via upload

---
+++ insertCustomer.java
@@ -0,0 +1,51 @@
+package DBConnect;
+
+import java.sql.*;
+
+public class insertCustomer {
+	// Incorporate MySQL driver
+	public static void insertCustomer(String first_name, String last_name, String cc_id, String address, String email, String password,Connection connection)throws Exception{
+    Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+   // Connect to the test database
+   connection =
+       DriverManager.getConnection("jdbc:mysql:///moviedb", "root", "liudl015");
+   
+   //if his credit card does not exist in the credit card table
+   try{
+   String checkcredit="SELECT * FROM creditcards WHERE id=?";
+   PreparedStatement updatecredit = connection.prepareStatement(checkcredit);
+   updatecredit.setString(1, cc_id);
+   ResultSet results = updatecredit.executeQuery();
+	results.next();
+	
+	if (results.getInt(1) < 0) return;
+	
+	}
+   catch (SQLException e)
+	{
+		System.out.println("credit card does not exist in the credit card table");
+		return;
+	}
+   
+   //If the customer has a single name
+   if(last_name==null){
+	   last_name=first_name;
+	   first_name="";
+   }
+   // prepare SQL statement template that's to be repeatedly excuted
+   String updateString = "INSERT INTO customers (first_name, last_name, cc_id, address, email, password) VALUES(?,?,?,?,?,?)";
+   PreparedStatement updateCustomers = connection.prepareStatement(updateString);
+
+   
+   updateCustomers.setString(1, first_name);
+   updateCustomers.setString(2, last_name);
+   updateCustomers.setString(3, cc_id);
+   updateCustomers.setString(4, address);
+   updateCustomers.setString(5, email);
+   updateCustomers.setString(6, password);
+
+           
+           updateCustomers.executeUpdate();
+       }
+}
---
+++ insertNewStar.java
@@ -0,0 +1,33 @@
+package DBConnect;
+
+import java.sql.*;
+
+public class insertNewStar {
+	
+	public static void insertStar(String first_name, String last_name, String dob,Connection connection) throws Exception{
+		
+		
+		// Incorporate MySQL driver
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+       // Connect to the test database
+       connection =
+           DriverManager.getConnection("jdbc:mysql:///moviedb", "root", "liudl015");
+
+       if(last_name==null){
+    	   last_name=first_name;
+    	   first_name="";
+       }
+       // prepare SQL statement template that's to be repeatedly excuted
+       String updateString = "INSERT INTO stars (first_name, last_name, dob) VALUES (?,?,?)";
+       PreparedStatement updateStars = connection.prepareStatement(updateString);
+
+       
+               updateStars.setString(1, first_name);
+               updateStars.setString(2, last_name);
+               updateStars.setString(3, dob);
+               
+               updateStars.executeUpdate();
+           }
+	}
+
