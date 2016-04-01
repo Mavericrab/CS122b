d33acfbc46725eca958c5ef50005dd0011dc1f4b
Mavericrab <dxinamoy@gmail.com>
2016-03-31 22:03:31 -0700

    Added files via upload

--- task2.java
+++ task2.java
@@ -43,7 +43,6 @@ public class task2 {
      * @connection
      * 
      */
-	
 	public static void queryStarByName(String first_name,String last_name,Connection connection) throws Exception{
 		Statement select = connection.createStatement();
 		
@@ -114,44 +113,88 @@ public class task2 {
      * @connection
      * 
      */
-    
-    public static void insertCustomer(String first_name, String last_name, String cc_id, String address, String email, String password,Connection connection)throws Exception{
-    	try{
-    		   String checkcredit="SELECT * FROM creditcards WHERE id=?";
-    		   PreparedStatement updatecredit = connection.prepareStatement(checkcredit);
-    		   updatecredit.setString(1, cc_id);
-    		   ResultSet results = updatecredit.executeQuery();
-    			results.next();
-    			
-    			if (results.getInt(1) < 0) return;
-    			
-    			}
-    		   catch (SQLException e)
-    			{
-    				System.out.println("credit card does not exist in the credit card table");
-    				return;
-    			}
-    		   
+    public static void insertCustomer(String first_name, String last_name, String cc_id, 
+    		String address, String email, String password,Connection connection)throws Exception{
+    	
+    		String checkCredit="SELECT COUNT(*) FROM creditcards WHERE id=?";
+    		PreparedStatement updateCredit = connection.prepareStatement(checkCredit);
+    		updateCredit.setString(1, cc_id);
+    		ResultSet results = updateCredit.executeQuery();
+    		if(results.next()){
+    		
     		   //If the customer has a single name
     		   if(last_name==null){
     			   last_name=first_name;
     			   first_name="";
     		   }
+    		   
+    		   if(first_name==null){
+    		   	first_name="";
+    		   }
+    		   
     		   // prepare SQL statement template that's to be repeatedly excuted
-    		   String updateString = "INSERT INTO customers (first_name, last_name, cc_id, address, email, password) VALUES(?,?,?,?,?,?)";
+    		   String updateString = "INSERT INTO customers (first_name, last_name, cc_id, " +
+    		   		"address, email, password) VALUES(?,?,?,?,?,?)";
     		   PreparedStatement updateCustomers = connection.prepareStatement(updateString);
 
-    		   
     		   updateCustomers.setString(1, first_name);
     		   updateCustomers.setString(2, last_name);
     		   updateCustomers.setString(3, cc_id);
     		   updateCustomers.setString(4, address);
     		   updateCustomers.setString(5, email);
     		   updateCustomers.setString(6, password);
-
-    		           
-    		           updateCustomers.executeUpdate();
-    		       
+               try{
+            	   updateCustomers.executeUpdate();
+    		       System.out.println("add succeeded");
+    		   }
+               catch (SQLException e){
+            	   System.out.println("credit card does not exist in the credit card table");
+            	   System.out.println(e.getMessage());
+			       return;
+		       }
+           }
     }
- 
+    
+    /**
+     * Delete a customer from the database. 
+     * @throws Exception 
+     * 
+     * @customer_ID
+     * @connection
+     * 
+     */
+    public static void deleteCustomer(int customer_ID, Connection connection) throws Exception{
+	           Statement update = connection.createStatement();
+	           System.out.println("delete from customers where id="+customer_ID);
+	           update.executeUpdate("delete from customers where id="+customer_ID);
+	
+    }
+    
+    /**
+     * Provide the metadata of the database; 
+     * in particular, print out the name of each table and, for each table, each attribute and its type. 
+     * @throws Exception 
+     * 
+     
+     * @connection
+     * 
+     */
+    public static void providemetadata(Connection connection) throws Exception{
+        String colName;
+        String colType;
+        String tabName;
+        
+    	DatabaseMetaData metadata = connection.getMetaData();
+    	ResultSet tableSet = metadata.getTables(null, "%", "%", new String[]{"TABLE"});
+    	while(tableSet.next()){
+    		tabName = tableSet.getString("TABLE_NAME");
+    		System.out.println(tabName+":");
+    		ResultSet colSet = metadata.getColumns(null, "%", tabName, "%");
+    		while(colSet.next()){
+    			colName = colSet.getString("COLUMN_NAME");
+    			colType = colSet.getString("TYPE_NAME");
+    			System.out.println("  "+colName+" "+colType);
+    		}
+    	}}
+
 }
